d7c777e6094604a2d98c8c9a0a392c803ef35937
Ruchi <ruchi.thanabhawan@gmail.com>
2025-01-27 18:22:47 -0800

    refactor

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,35 @@
 import "dotenv/config";
-import express from "express"; 
+import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 
 import mongoose from "mongoose";
 
-import authRouter from "./routes/auth.js"
-import menuRouter from "./routes/menus.js"
-import orderRouter from "./routes/orders.js"
+import authRouter from "./routes/auth.js";
+import menuRouter from "./routes/menus.js";
+import orderRouter from "./routes/orders.js";
 
 const port = process.env.PORT;
 
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => console.log("Connected to DB"))
-  .catch((err) => console.error("Failed to connect to DB:", err));
+(async () => {
+  try {
+    await mongoose
+      .connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`)
+      .then(() => console.log("Connected to DB"));
+  } catch (error) {
+    console.error("Failed to connect to DB:", error);
+    throw error;
+  }
+})();
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
 
 app.use("/auth", authRouter);
 app.use("/menu", menuRouter);
-app.use("/order", orderRouter)
-
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-  }); 
-
+app.use("/order", orderRouter);
